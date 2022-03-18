9bd1dc00862203a99f6e1b28bcf14668b6e539cb
Pedro Henrique Faria Teixeira <pedro.teixeira@ibm.com>
2022-03-18 15:00:03 -0300

    First commit

---
+++ data-engineer/module-2/noticia_coleta/noticia_coleta/spiders/coletanoticia.py
@@ -0,0 +1,10 @@
+import scrapy
+
+
+class ColetanoticiaSpider(scrapy.Spider):
+    name = 'coletanoticia'
+    allowed_domains = ['https://www1.folha.uol.com.br/ultimas-noticias/#10']
+    start_urls = ['http://https://www1.folha.uol.com.br/ultimas-noticias/#10/']
+
+    def parse(self, response):
+        pass
